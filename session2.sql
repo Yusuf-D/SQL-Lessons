8e6ee46cb57f42ca41c068efe0d3b95fe87af544
Yusuf_D <ydemirci7@gmail.com>
2021-12-11 13:58:50 +0300

    session2 modified

--- session2.sql
+++ session2.sql
@@ -1,30 +1,40 @@
-/*===================================================
-														LIMIT
-====================================================*/
- 	/*invoices tablosunda Total değeri 10$'dan büyük olan ilk 4 kayıt'ın InvoiceId, 
-	InvoiceDate ve total bilgilerini sorgulayiniz */
-	SELECT InvoiceId, InvoiceDate,total
-	FROM invoices
-	WHERE total >10
-	LIMIT 4;
+/*=================================================================================================
+                                       LIMIT
+==================================================================================================*/
+/*invoices tablosunda Total değeri 10$'dan büyük olan ilk 4 kayıt'ın InvoiceId,
+InvoiceDate ve total bilgilerini sorgulayiniz */
 	
+SELECT InvoiceId, InvoiceDate, total
+FROM invoices
+WHERE total > 10
+LIMIT 4;
+/*=================================================================================================
+                                       ORDER BY
+==================================================================================================*/
+/*invoices tablosunda Total değeri 10$'dan büyük olan kayıtları Total değerine göre
+ARTAN sırada sıralayarak sorgulayiniz */
 	
-/*===================================================
-													ORDER BY
-====================================================*/
-	
-	/*invoices tablosunda Total değeri 10$'dan büyük olan kayıtları Total değerine göre 
-	ARTAN sırada sıralayarak sorgulayiniz */
-	SELECT *
-	FROM invoices
-	WHERE total >10
-	ORDER BY total ASC;  -- ORder By komutunu default değeri ASC dir.
-	
-	/*invoices tablosunda Total değeri 10$'dan büyük olan kayıtlardan işlem tarihi 
-	(InvoiceDate) en yeni olan 10 kaydın tüm bilgilerini listeyiniz */ 
-	
-	SELECT *
-	FROM invoices
-	WHERE total >10
-	ORDER BY InvoiceDate DESC
-	LIMIT 10;
+SELECT total FROM invoices WHERE total > 10 ORDER BY total ASC; --ORDER BY command has the default as ASC
+/*invoices tablosunda Total değeri 10$'dan büyük olan kayıtlardan işlem tarihi
+(InvoiceDate) 	en yeni olan 10 kaydın tüm bilgilerini listeyiniz */
+SELECT InvoiceDate FROM invoices WHERE total > 10 ORDER BY InvoiceDate DESC LIMIT 10;
+/*=================================================================================================
+                                       AND, OR, NOT
+==================================================================================================*/
+/* invoices tablosunda ülkesi (BillingCountry) USA olmayan kayıtları total değerine
+göre  AZALAN sırada listeyiniz */
+SELECT *
+FROM invoices
+WHERE NOT BillingCountry = 'USA' --"WHERE NOT BillingCountry <> 'USA'" kullanırsak aynı manada gelir
+ORDER BY total DESC;
+/* invoices tablosunda, ülkesi (BillingCountry) USA veya Germany olan kayıtları,
+InvoiceId sırasına göre artan sırada listeyiniz */
+SELECT *
+FROM invoices
+WHERE BillingCountry = 'USA' OR BillingCountry = 'Germany'
+ORDER BY InvoiceId ASC;
+/* invoices tablosunda fatura tarihi (InvoiceDate) 01-01-2012 ile 03-01-2012
+tarihleri arasındaki faturaların tüm bilgilerini listeleyiniz */
+SELECT *
+FROM invoices
+WHERE InvoiceDate >= '2012-01-01' AND InvoiceDate <= '2012-01-03';
